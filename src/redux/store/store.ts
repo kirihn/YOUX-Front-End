6afdb92c571984d64f65a29519bb7a4ac17218d3
Zukay Kiryl <kzykov03@mail.ru>
2025-07-20 18:59:37 +0300

    feat: add slices to store

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import CurrentUserIdReducer from '../slices/CurrentUserSlice';
+import pageIndexReducer from '../slices/pageIndexSlice';
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    CurrentUserId: CurrentUserIdReducer,
+    pageIndex: pageIndexReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
