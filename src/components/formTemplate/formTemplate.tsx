a84993f8668cbad11b99c4c9bfc95043198ac70e
Zukay Kiryl <kzykov03@mail.ru>
2025-07-20 18:33:06 +0300

    feat: add stelys for form template

--- src/components/formTemplate/formTemplate.tsx
+++ src/components/formTemplate/formTemplate.tsx
@@ -1,53 +1,58 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import type { Props } from './types';
+import './formTemplate.scss';
 export function FormTemplate<T extends object>(props: Props<T>) {
   return (
-    <Formik
-      initialValues={props.initialValues}
-      validationSchema={props.validationSchema}
-      onSubmit={props.onSubmit}
-    >
-      {({ setFieldValue }) => (
-        <Form>
-          {props.fields.map((field) => (
-            <div className="inputContainer" key={field.name}>
-              <label htmlFor={field.name}>{field.label}</label>
-              <ErrorMessage
-                name={field.name}
-                component="div"
-                className="form-error"
-              />
-              {field.type === 'select' && field.options ? (
-                <Field as="select" name={field.name}>
-                  <option value="">Выберите...</option>
-                  {field.options.map((opt) => (
-                    <option key={opt.value} value={opt.value}>
-                      {opt.label}
-                    </option>
-                  ))}
-                </Field>
-              ) : field.type === 'file' ? (
-                <input
+    <div className="formContainer">
+      <Formik
+        initialValues={props.initialValues}
+        validationSchema={props.validationSchema}
+        onSubmit={props.onSubmit}
+      >
+        {({ setFieldValue }) => (
+          <Form className="formTemplate">
+            {props.fields.map((field) => (
+              <div className="inputContainer" key={field.name}>
+                <label htmlFor={field.name}>{field.label}</label>
+                <ErrorMessage
                   name={field.name}
-                  type="file"
-                  onChange={(e) => {
-                    if (e.currentTarget.files && e.currentTarget.files[0]) {
-                      setFieldValue(field.name, e.currentTarget.files[0]);
-                    }
-                  }}
+                  component="div"
+                  className="form-error"
                 />
-              ) : (
-                <Field
-                  name={field.name}
-                  type={field.type}
-                  placeholder={field.placeHolder}
-                />
-              )}
-            </div>
-          ))}
-          <button type="submit">{props.submitText}</button>
-        </Form>
-      )}
-    </Formik>
+                {field.type === 'select' && field.options ? (
+                  <Field as="select" name={field.name}>
+                    <option value="">Выберите...</option>
+                    {field.options.map((opt) => (
+                      <option key={opt.value} value={opt.value}>
+                        {opt.label}
+                      </option>
+                    ))}
+                  </Field>
+                ) : field.type === 'file' ? (
+                  <input
+                    name={field.name}
+                    type="file"
+                    onChange={(e) => {
+                      if (e.currentTarget.files && e.currentTarget.files[0]) {
+                        setFieldValue(field.name, e.currentTarget.files[0]);
+                      }
+                    }}
+                  />
+                ) : (
+                  <Field
+                    name={field.name}
+                    type={field.type}
+                    placeholder={field.placeHolder}
+                  />
+                )}
+              </div>
+            ))}
+            <button type="submit" className="submitButton">
+              {props.submitText}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
--- src/pages/createUserPage/createUserPage.tsx
+++ src/pages/createUserPage/createUserPage.tsx
@@ -1,12 +1,14 @@
 import { FormTemplate } from '../../components/formTemplate/formTemplate';
 import { createUserValidationSchema } from '../../utils/yup/createUserSchema';
 import { initialValues, createUserFields } from './types';
+import './createUserPage.scss'
 export function CreateUserPage() {
   const handleSubmit = (values: typeof initialValues) => {
     const formData = new FormData();
     Object.entries(values).forEach(([key, value]) => {
       formData.append(key, value as string | Blob);
     });
+
   };
   return (
     <div className="createUserPageContainer">
@@ -15,7 +17,7 @@ export function CreateUserPage() {
         fields={createUserFields}
         validationSchema={createUserValidationSchema}
         onSubmit={handleSubmit}
-        submitText="Добавить"
+        submitText="Создать пользователя"
       />
     </div>
   );
